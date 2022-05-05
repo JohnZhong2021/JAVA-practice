1b77aae6194afc11a438dc0f0deb0d0a0d49819a
JohnZhong <wpzhongqiang@hotmail.com>
2022-05-05 21:08:12 +0800

    generate random int number

---
+++ randomNumber.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+public class randomNumber {
+    public void generateRandomint(int n) {
+        Random r = new Random();
+        for (int i = 0; i < n; i++) {
+            int r1 = r.nextInt(20) + 1;
+            System.out.println(r1);
+        }
+    }
+
+    public static void main(String[] args) {
+        randomNumber rn = new randomNumber();
+        rn.generateRandomint(10);
+
+    }
+}
